dfa1652970c961a7a3801cd13711dd006504acf3
BryanTeruel <164897126+BryanTeruel@users.noreply.github.com>
2025-01-27 21:22:33 -0700

    week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <>
+        <h1>My Name is Bryan Benedict Teruel</h1>
+        <h2>Visit my github in this <Link href="https://github.com/BryanTeruel/cprg306-assignments">link</Link>. </h2>
+        </>
+    );
+}
