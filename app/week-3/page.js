7a6849c422ad7c571cc56b9c3074c44ac3d4918f
BryanTeruel <164897126+BryanTeruel@users.noreply.github.com>
2025-02-02 22:00:01 -0700

    week 3 assignment
    props and TailWind css

---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+    return(
+        <main className="p-8 bg-sky-950">
+            <h1 className="text-5xl font-bold mb-6">Shopping List</h1>
+            <ItemList/>
+        </main>
+    );
+}
